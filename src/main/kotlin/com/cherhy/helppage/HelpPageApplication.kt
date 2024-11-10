c4edd7714db40d837dd8d42bbae59780e152158f
lolmageap <ekxk1234@naver.com>
2024-11-10 14:33:11 +0900

    project init

---
+++ src/main/kotlin/com/cherhy/helppage/HelpPageApplication.kt
@@ -0,0 +1,11 @@
+package com.cherhy.helppage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelpPageApplication
+
+fun main(args: Array<String>) {
+    runApplication<HelpPageApplication>(*args)
+}
---
+++ src/test/kotlin/com/cherhy/helppage/HelpPageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cherhy.helppage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelpPageApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
