758705901d3ab13af1f3abed1cda0d206c5564b0
lolmageap <ekxk1234@naver.com>
2024-11-13 20:50:12 +0900

    refactor controllers

--- src/main/kotlin/com/cherhy/helppage/HelpPageApi.kt
+++ src/main/kotlin/com/cherhy/helppage/HelpPageApi.kt
@@ -1,29 +1,20 @@
 package com.cherhy.helppage
 
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelpPageApi(
     private val helpPage: HelpPage,
 ) {
-    @PostMapping("/help/{uuid}")
+    @PostMapping("/help")
     fun helpPage(
-        @PathVariable uuid: String,
+        @RequestBody request: HelpMessage,
     ) {
-        require(uuid == Randomizer.uuid) { "Invalid UUID" }
-        val email = helpPage.contact.email
-        TODO("여기서 email을 보내는 로직을 작성해야함.")
-        /**
-         * 실제로 이메일을 보내는게 아닌 내가 만든 특정 수신 서버에서 api 호출을 받고 그 수신 서버에서 이메일을 보내는 로직을 작성해야함.
-         * 서버가 init 될 때 uuid를 생성하고 내 수신 서버에서는 이 uuid 값을 보낸 사용자만 허용하자.
-         * application이 꺼진다면 uuid 삭제 call api를 만들어서 수신 서버에서 삭제하자.
-         */
+        helpPage.saveHelpMessage(
+            userId = request.userId,
+            message = request.message,
+        )
     }
-}
-
-object Randomizer {
-    const val UUID = "uuid"
-    val uuid: String = java.util.UUID.randomUUID().toString()
 }
--- src/main/kotlin/com/cherhy/helppage/HelpPageController.kt
+++ src/main/kotlin/com/cherhy/helppage/HelpPageController.kt
@@ -1,7 +1,5 @@
 package com.cherhy.helppage
 
-import com.cherhy.helppage.Randomizer.UUID
-import com.cherhy.helppage.Randomizer.uuid
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,20 +12,11 @@ class HelpPageController(
     fun helpPage(
         model: Model,
     ): String {
-        with(model) {
-            addAttribute(VERSION, helpPage.version)
-            addAttribute(URL, helpPage.url)
-            addAttribute(CONTACT, helpPage.contact)
-            addAttribute(PAGE_DETAIL, helpPage.pageDetail)
-            addAttribute(UUID, uuid)
-        }
+        model.addAttribute(PAGE_DETAIL, helpPage.pageDetail)
         return "help"
     }
 
     companion object {
-        const val VERSION = "version"
-        const val URL = "url"
-        const val CONTACT = "contact"
         const val PAGE_DETAIL = "pageDetail"
     }
 }
